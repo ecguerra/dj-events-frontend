612bb22347d65e72ea551e9d67b1868e3cd462e2
Elizabeth Guerra <ecguerra@outlook.com>
2021-09-15 18:56:36 -0400

    pages and routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+// the only place you can import global style sheets is in _app.js
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
