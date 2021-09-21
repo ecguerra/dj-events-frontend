a223fd5e890b2831b933f66431d6199385b23a13
Elizabeth Guerra <ecguerra@outlook.com>
2021-09-21 16:34:50 -0400

    custom 404 built

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import { FaExclamationTriangle } from 'react-icons/fa'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+
+export default function NotFound() {
+    return (
+        <Layout title='Page Not Found'>
+            <div className={styles.error}>
+                <h1><FaExclamationTriangle /> 404'd!</h1>
+                <h4>Nooo my browser!</h4>
+                <Link href='/'>Go back home</Link>
+            </div>
+        </Layout>
+    )
+}
