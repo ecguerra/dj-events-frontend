65061669a817b404cea7d1d191983612324547a5
Elizabeth Guerra <ecguerra@outlook.com>
2021-09-21 16:22:16 -0400

    Header & Footer created

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styles from '../styles/Header.module.css'
+
+// Link uses href instead of 'to' in react-router-dom
+// 'a' tag technically isn't needed anymore, but is useful if you want to style
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Link href='/'>
+                    <a>DJ Events</a>
+                </Link>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href='/events'>
+                            <a>Events</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
