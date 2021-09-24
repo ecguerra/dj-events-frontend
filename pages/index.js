1c36eb6a35c62c54b5c572709df4891d2e2d6ffb
Elizabeth Guerra <ecguerra@outlook.com>
2021-09-23 21:37:26 -0400

    getStaticProps and API_URL variables added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
 
-export default function Home() {
+// getServerSideProps runs every time we come to the page
+// getStaticProps makes request at build time and builds out static pages
+// drawback is that if there are updates or changes they won't render 
+// revalidate is workaround - checks for updates
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  // have to pass to the client here
+  return {
+    props: {events},
+    revalidate: 1
+  }
+
+}
+
+// and pass in as a prop here
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
 }
