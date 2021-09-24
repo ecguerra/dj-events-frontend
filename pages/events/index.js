a8d5d9937c5cae26ba63399fd3ba71c1f31ef317
Elizabeth Guerra <ecguerra@outlook.com>
2021-09-23 21:54:49 -0400

    display events using dev API

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,35 @@
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import {API_URL} from '@/config/index'
 
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>My Events</h1>
-        </Layout>
-    )
+// getServerSideProps runs every time we come to the page
+// getStaticProps makes request at build time and builds out static pages
+// drawback is that if there are updates or changes they won't render 
+// revalidate is workaround - checks for updates
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  // have to pass to the client here
+  return {
+    props: {events},
+    revalidate: 1
+  }
+
 }
+
+// and pass in as a prop here
+export default function EventsPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt}/>
+      ))}
+    </Layout>
+  )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
 import {API_URL} from '@/config/index'
 
 // getServerSideProps runs every time we come to the page
@@ -12,7 +14,7 @@ export async function getStaticProps() {
 
   // have to pass to the client here
   return {
-    props: {events},
+    props: {events:events.slice(0, 3)},
     revalidate: 1
   }
 
@@ -23,6 +25,17 @@ export default function Home({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt}/>
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
