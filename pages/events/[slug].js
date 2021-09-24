0fba21fbd5f290452dbb768d2124711788b8c899
Elizabeth Guerra <ecguerra@outlook.com>
2021-09-23 22:26:08 -0400

    built out single event page with getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,54 @@
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
 
-export default function EventPage() {
+export default function EventPage({evt}) {
     return (
         <Layout>
-            <h1>My Event</h1>
+            <h1>{evt.name}</h1>
         </Layout>
     )
 }
+
+// since these are not set urls, we have to use getStaticPaths
+export async function getStaticPaths() {
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+
+    const paths = events.map(evt => ({
+        params: {slug: evt.slug}
+    }))
+
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+
+// this won't work on its own in this case - needs getStaticPaths
+export async function getStaticProps({ params: {slug} }) {
+    const res = await fetch(`${API_URL}/api/events/${slug}`)
+
+    const events = await res.json()
+
+    return {
+        props: {
+            evt: events[0]
+        },
+        revalidate: 1
+    }
+}
+
+// // example using getServerSideProps
+
+// export async function getServerSideProps({ query: {slug} }) {
+//     const res = await fetch(`${API_URL}/api/events/${slug}`)
+
+//     const events = await res.json()
+
+//     return {
+//         props: {
+//             evt: events[0]
+//         },
+//     }
+// }
